cfcaf42d77fad7913b33bd26e137492693e34d56
malej <matt.malej@gmail.com>
2017-08-14 09:24:28 -0500

    [WIP] adding changes to Makefiles and autoconf tools preinstall script needed for hdf5 and netcdf

--- Makefile
+++ Makefile
@@ -13,13 +13,10 @@ export
 #endif
 
 
-
 .PHONY: all clean cleaner help
 
 
-
-
-all:	version_select verify_config_files install_external_packages 
+all:	version_select verify_config_files install_externalPackages install_funwave
 
 
 verify_config_files:
@@ -37,16 +34,25 @@ version_select:
 check_all:
 	cd externalPackages && make check_all
 
-#testGlobalConfig:
-#		@test -f versionsConfig/version.${FUNWAVE_ARCH}|| { echo "Global config file does not exist for current version: '"${FUNWAVE_ARCH}"'. Exiting..." ; exit 1; }
 
-install_FUNWAVE:
+# installs
+install_funwave:
 	${eval TITLE=Building FUNWAVE-TVD}
 	@cd scripts && ./printTitleCmd.sh
 	@cd src && make
 
-install_external_packages:
+install_externalPackages:
 	@cd externalPackages; $(MAKE) -s all
 
+
+# cleans
 clean_externalPackages: 
 	cd externalPackages && make -k distclean
+
+clean_funwave:
+	cd src && make clean
+
+clean:	clean_funwave clean_externalPackages
+
+cleaner: 
+	cd src && make clobber
---
+++ externalPackages/autoconf_tools/preinstall_autoconf_tools.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+#installs autoconf, automake, libtool to the current directory (installs in a directory called 'util')
+
+CC=gcc
+
+AUTOCONF_VERS=autoconf-2.69
+AUTOMAKE_VERS=automake-1.15
+LIBTOOL_VERS=libtool-2.4.6
+
+#
+# Should not need to modify any lines below
+#
+INSTALL_DIR=$(FUNWAVE_PREFIX)
+export PATH="${INSTALL_DIR}/util/bin:${PATH}"
+
+wget http://ftp.gnu.org/gnu/autoconf/$AUTOCONF_VERS.tar.gz
+tar xzf $AUTOCONF_VERS.tar.gz
+cd $AUTOCONF_VERS
+./configure --prefix=$INSTALL_DIR/util CC=$CC
+make
+status=$?
+if [[ $status != 0 ]]; then
+  echo "AUTOCONF FAILED: make"
+  exit $status
+fi
+make install
+status=$?
+if [[ $status != 0 ]]; then
+  echo "AUTOCONF FAILED: make install"
+  exit $status
+fi
+cd ..
+
+wget http://ftp.gnu.org/gnu/automake/$AUTOMAKE_VERS.tar.gz
+tar xzf $AUTOMAKE_VERS.tar.gz
+cd $AUTOMAKE_VERS
+./configure --prefix=$INSTALL_DIR/util CC=$CC
+make
+status=$?
+if [[ $status != 0 ]]; then
+  echo "AUTOMAKE FAILED: make"
+  exit $status
+fi
+make install
+status=$?
+if [[ $status != 0 ]]; then
+  echo "AUTOMAKE FAILED: make install"
+  exit $status
+fi
+cd ..
+
+wget http://ftp.gnu.org/gnu/libtool/$LIBTOOL_VERS.tar.gz
+tar xzf $LIBTOOL_VERS.tar.gz
+cd $LIBTOOL_VERS
+./configure --prefix=$INSTALL_DIR/util CC=$CC
+make
+status=$?
+if [[ $status != 0 ]]; then
+  echo "LIBTOOL FAILED: make"
+  exit $status
+fi
+make install
+status=$?
+if [[ $status != 0 ]]; then
+  echo "LIBTOOL FAILED: make install"
+  exit $status
+fi
+cd ..
+
+#rm -fr autoconf* automake* libtool* util
+#PATH=$INSTALL_DIR/util/bin:$PATH
+
+NO_COLOR='\033[0m'
+GREEN_COLOR='\033[32;01m'
+RED_COLOR='\033[31;01m'
+
+printf "\n${GREEN_COLOR}   *** YOU NEED TO ADD THE util DIR TO PATH ****\n       In bash: ${RED_COLOR} export PATH=\"${INSTALL_DIR}/util/bin:\$PATH\" ${GREEN_COLOR} ${GREEN_COLOR} \n       In csh: ${RED_COLOR} setenv PATH \"${INSTALL_DIR}/util/bin:\$PATH\" ${NO_COLOR} \n\n"
