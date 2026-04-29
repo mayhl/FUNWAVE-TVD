9afe7c615cfcfc20adb9b524c41de8f90fd8f694
mayhl <michaelangelo.yh.lam@gmail.com>
2026-04-29 00:44:17 -0500

    build: replace flogging and ffilesystem with native modules

--- src/core/logging.f90
+++ src/core/logging.f90
@@ -1,47 +1,32 @@
-!--------------------------------------------------
-!   This file is part of the FUNWAVE-TVD
-!   program under the Simplified BSD license
-!--------------------------------------------------
-!
-!  Class for handling pretty logging
-!  https://github.com/BoldingBruggeman/fortran-yaml
-!
-!  PURPOSE:
-!   - interface with external library
-!   - handle code termination on errors
-!
-!  Authors:
-!   [mayhl] Michael-Angelo Y.-H. Lam
-!
-!  HISTORY:
-!    11/23/2025 [mayhl] created initial class
-!
-!-------------------------------------------------
-
+!> @brief Module for platform-independent logging.
+!! Replaces external flogging dependency with a native implementation.
 module core_log_io_mod
-   use core_constants_mod, only: MESSAGE_SIZE, STRING_SIZE, LABEL_SIZE
-   use logger_mod, only: logger_init => logger_init, logger => master_logger
-   use core_throw_mod, only: throw_exception, set_error_code
-
-   implicit none(external)
-
+   use, intrinsic :: iso_fortran_env, only: output_unit, error_unit
+   use face, only: color_text, COLOR_RED, COLOR_YELLOW, COLOR_BLUE, COLOR_RESET
+   implicit none
    private
 
-   public :: type_log_writer, new_log_writer, finalize_logger
+   public :: type_log_writer, new_log_writer
 
-   type type_log_writer
+   !> @brief Log levels
+   integer, parameter, public :: log_level_debug = 1
+   integer, parameter, public :: log_level_info = 2
+   integer, parameter, public :: log_level_warn = 3
+   integer, parameter, public :: log_level_error = 4
+   integer, parameter, public :: log_level_fatal = 5
+
+   type, public :: type_log_writer
       private
-      CHARACTER(LABEL_SIZE) :: label
+      character(len=20) :: label
       logical :: is_io_node = .false.
+      integer :: min_stdout_level = log_level_info
+      integer :: min_stderr_level = log_level_error
+      integer :: file_unit = -1
    contains
-      private
-      procedure, public :: debug
-      procedure, public :: trivia
-      procedure, public :: info
-      procedure, public :: warning
-      procedure, public :: exit_on_error
-      procedure, public :: exit_on_fatal
-      procedure, public :: finalize => log_writer_finalize
+      procedure, public :: debug, trivia, info, warning => warn, exit_on_error, exit_on_fatal, finalize => log_writer_finalize
+      procedure, public :: set_levels => log_set_levels
+      procedure, public :: set_file => log_set_file
+      procedure, private :: write_log
    end type type_log_writer
 
    interface new_log_writer
@@ -50,112 +35,105 @@ module core_log_io_mod
 
 contains
 
-   function type_log_writer_initialize(label, is_io_node, path, std_err_threshold, std_out_threshold, logfile_threshold) result(this)
+  function type_log_writer_initialize(label, is_io_node, path, std_err_threshold, std_out_threshold, logfile_threshold) result(this)
       character(*), intent(in) :: label
       logical, intent(in) :: is_io_node
-      character(*), intent(in), optional ::  path
-      integer, optional, intent(in) :: std_err_threshold
-      integer, optional, intent(in) :: std_out_threshold
-      integer, optional, intent(in) :: logfile_threshold
-
+      character(*), intent(in), optional :: path
+      integer, optional, intent(in) :: std_err_threshold, std_out_threshold, logfile_threshold
       type(type_log_writer) :: this
 
-      character(*), parameter:: default_log_path = "new.log"
-      character(len=:), allocatable :: log_path
-
-      if (present(path)) then
-         log_path = path
-         call logger_init(trim(log_path), &
-                          stderr_threshold=std_err_threshold, &
-                          stdout_threshold=std_out_threshold, &
-                          logfile_threshold=logfile_threshold)
-      end if
-
-      this%is_io_node = is_io_node
       this%label = label
-
+      this%is_io_node = is_io_node
+      if (present(std_out_threshold)) this%min_stdout_level = std_out_threshold
+      if (present(std_err_threshold)) this%min_stderr_level = std_err_threshold
+      if (present(path)) call this%set_file(path)
    end function type_log_writer_initialize
 
-   ! Log debugging info and continue run
-   subroutine debug(this, message)
+   subroutine log_set_levels(this, stdout_level, stderr_level)
+      class(type_log_writer), intent(inout) :: this
+      integer, intent(in), optional :: stdout_level, stderr_level
+      if (present(stdout_level)) this%min_stdout_level = stdout_level
+      if (present(stderr_level)) this%min_stderr_level = stderr_level
+   end subroutine log_set_levels
 
+   subroutine log_set_file(this, filename)
+      class(type_log_writer), intent(inout) :: this
+      character(len=*), intent(in) :: filename
+      integer :: stat
+      if (this%file_unit /= -1) close (this%file_unit)
+      open (newunit=this%file_unit, file=trim(filename), status='replace', action='write', iostat=stat)
+   end subroutine log_set_file
+
+   subroutine debug(this, message)
       class(type_log_writer), intent(inout) :: this
       character(len=*), intent(in) :: message
-
-      if (this%is_io_node) then
-         call logger%debug(trim(this%label), trim(message))
-      end if
+      call this%write_log(log_level_debug, "DEBUG", message)
    end subroutine debug
 
-   ! Log trivia and continue run
    subroutine trivia(this, message)
-
       class(type_log_writer), intent(inout) :: this
       character(len=*), intent(in) :: message
-
-      if (this%is_io_node) then
-         call logger%trivia(trim(this%label), trim(message))
-      end if
+      call this%write_log(log_level_debug, "TRIVIA", message)
    end subroutine trivia
 
-   ! Log info and continue run
    subroutine info(this, message)
-
       class(type_log_writer), intent(inout) :: this
       character(len=*), intent(in) :: message
-
-      if (this%is_io_node) then
-         call logger%info(trim(this%label), trim(message))
-      end if
+      call this%write_log(log_level_info, "INFO", message)
    end subroutine info
 
-   ! Log warning and continue run
-   subroutine warning(this, message)
-
+   subroutine warn(this, message)
       class(type_log_writer), intent(inout) :: this
       character(len=*), intent(in) :: message
+      call this%write_log(log_level_warn, "WARN", message)
+   end subroutine warn
 
-      if (this%is_io_node) then
-         call logger%warning(trim(this%label), trim(message))
-      end if
-
-   end subroutine warning
-
-   ! Log error and exit run
    subroutine exit_on_error(this, message, errcode)
       class(type_log_writer), intent(inout) :: this
       character(len=*), intent(in) :: message
       integer, optional, intent(in) :: errcode
-
-      if (this%is_io_node) then
-         call logger%error(trim(this%label), trim(message))
-         call set_error_code(errcode)
-         call throw_exception(__FILE__, __LINE__, message=message)
-      end if
-
+      call this%write_log(log_level_error, "ERROR", message)
+      stop 1
    end subroutine exit_on_error
 
-   ! Log fatal error and exit run
    subroutine exit_on_fatal(this, message, errcode)
-
       class(type_log_writer), intent(inout) :: this
       character(len=*), intent(in) :: message
       integer, optional, intent(in) :: errcode
-
-      if (this%is_io_node) then
-         call logger%fatal(trim(this%label), trim(message))
-         call throw_exception(__FILE__, __LINE__, message=message)
-      end if
-
+      call this%write_log(log_level_fatal, "FATAL", message)
+      stop 1
    end subroutine exit_on_fatal
+
+   subroutine write_log(this, level, prefix, msg)
+      class(type_log_writer), intent(in) :: this
+      integer, intent(in) :: level
+      character(len=*), intent(in) :: prefix, msg
+
+      character(len=20) :: date, time
+      character(len=8)  :: zone
+      character(len=100) :: colored_prefix
+
+      if (.not. this%is_io_node) return
+
+      ! Get timestamp
+      call date_and_time(date, time, zone)
+
+      ! Apply colors using FACE
+      select case (level)
+      case (log_level_debug); colored_prefix = color_text(prefix, COLOR_BLUE)
+      case (log_level_warn); colored_prefix = color_text(prefix, COLOR_YELLOW)
+      case (log_level_error, log_level_fatal); colored_prefix = color_text(prefix, COLOR_RED)
+      case default; colored_prefix = prefix
+      end select
+
+      ! Formatting: [YYYY-MM-DD HH:MM:SS] [LABEL] [PREFIX] MSG
+      write(output_unit, '(A)') "["//date(1:4)//"-"//date(5:6)//"-"//date(7:8)//" "//time(1:2)//":"//time(3:4)//":"//time(5:6)//"] ["//trim(this%label)//"] ["//trim(colored_prefix)//"] "//trim(msg)
+      if (this%file_unit /= -1) write(this%file_unit, *) "["//date(1:4)//"-"//date(5:6)//"-"//date(7:8)//" "//time(1:2)//":"//time(3:4)//":"//time(5:6)//"] ["//trim(this%label)//"] ["//trim(prefix)//"] "//trim(msg)
+   end subroutine write_log
 
    subroutine log_writer_finalize(this)
       class(type_log_writer), intent(inout) :: this
-      call finalize_logger()
+      if (this%file_unit /= -1) close (this%file_unit)
    end subroutine log_writer_finalize
-
-   subroutine finalize_logger()
-      call logger%destroy()
-   end subroutine finalize_logger
 
 end module core_log_io_mod
